f32eb915f9a5970ba79b36c1a3c154c5accb9e07
Satriafuady <46353164+Satriafuady@users.noreply.github.com>
2019-01-25 15:53:41 +0700

    Update random.js

--- random.js
+++ random.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2019/01/tips-to-choose-best-erp-software-for.html"}
 if(x=="2"){xxx="/2019/01/five-common-gaps-to-watch-for-in-your.html"}
 if(x=="3"){xxx="/2019/01/how-erp-software-may-be-affected-by-end.html"}
 if(x=="4"){xxx="/2019/01/five-erp-software-trends-to-watch-in.html"}
-if(x=="5"){xxx="/2019/01/putting-e-enterprise-back-into-erp.html"}a_to_ve[i].href="http://www.new-erp-software.xyz/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2019/01/putting-e-enterprise-back-into-erp.html"}a_to_ve[i].href="http://www.new-erp-software.xyz"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
